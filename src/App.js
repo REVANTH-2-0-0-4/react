bdfa73a2f9137c8f1632d5ac0cec7d7f61402822
REVANTH-2-0-0-4 <revanthkurapati56@gmail.com>
2024-10-02 16:30:22 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React,{useState,useCallback} from "react";
+import React,{useState,useCallback,useEffect} from "react";
 import Button from "./Button";
 import Text from "./Text";
 
 const App = ()=>{
-  const[message,updatemessage] = useState(" hello user , good morning");
-   const revanth = useCallback( () =>{
-    updatemessage("hello user , good afternoon ");
-   },[])
-    return (
-      <>
-       <div>{message}</div>
-       <Button revanth={revanth}>Change Message ..!</Button>
-      </>
-    );
-    
+
+
+      
 }
 export default App;
 // named export s : export {App};
